b27ef813bdb518e87c2ae81a3964a10f234b129c
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-02-05 23:40:47 +0900

    fix some output formats

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdio.h>
+#include <inttypes.h>
 #include "mapmem.h"
 
 #define gt(n, sr) ((((1<<((n)))-1)<<((sr))))
@@ -12,12 +13,12 @@ int main()
 	p=mapmem();
 
 	printf("p: %p\n", p);
-	printf("p[0]: 0x%x\n", p[0]);
+	printf("p[0]: 0x%08"PRIx32"\n", p[0]);
 
-	printf("V3D Technology Version: %u\n", en(0, 31-24+1, 24));
-	printf("V3D Id String: 0x%x 0x%x 0x%x\n", en(0, 8, 16), en(0, 8, 8), en(0, 8, 0));
+	printf("V3D Technology Version: %"PRIu32"\n", en(0, 31-24+1, 24));
+	printf("V3D Id String: 0x%02"PRIx8" 0x%02"PRIx8" 0x%02"PRIx8"\n", en(0, 8, 16), en(0, 8, 8), en(0, 8, 0));
 	printf("V3D Id String: %c%c%c\n", en(0, 8, 16), en(0, 8, 8), en(0, 8, 0));
-	printf("VPM Memory Size: %u\n", en(1, 4, 28));
+	printf("VPM Memory Size: %"PRIu32"\n", en(1, 4, 28));
 
 	return 0;
 }
