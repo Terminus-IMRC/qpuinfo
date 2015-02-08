5b6cba6c45d1ec46d90b2475171d06e9accbc186
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-02-08 14:01:09 +0900

    delete V3D Id String in hex

--- main.c
+++ main.c
@@ -17,7 +17,6 @@ int main()
 	printf("p[0]: 0x%08"PRIx32"\n", p[0]);
 
 	printf("V3D Technology Version: %"PRIu32"\n", rf(0, 31, 24));
-	printf("V3D Id String: 0x%02"PRIx8" 0x%02"PRIx8" 0x%02"PRIx8"\n", rf(0, 7, 0), rf(0, 15, 8), rf(0, 23, 16));
 	printf("V3D Id String: %c%c%c\n", rf(0, 7, 0), rf(0, 15, 8), rf(0, 23, 16));
 
 	printf("VPM Memory Size: %"PRIu32"\n", rf(1, 31, 28));
