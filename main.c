81244f1e5b62879211a2e22e422212e89fe06943
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-02-08 13:29:46 +0900

    add printfs for V3D_IDENT1 register

--- main.c
+++ main.c
@@ -19,7 +19,14 @@ int main()
 	printf("V3D Technology Version: %"PRIu32"\n", rf(0, 31, 24));
 	printf("V3D Id String: 0x%02"PRIx8" 0x%02"PRIx8" 0x%02"PRIx8"\n", rf(0, 7, 0), rf(0, 15, 8), rf(0, 23, 16));
 	printf("V3D Id String: %c%c%c\n", rf(0, 7, 0), rf(0, 15, 8), rf(0, 23, 16));
+
 	printf("VPM Memory Size: %"PRIu32"\n", rf(1, 31, 28));
+	printf("HDR Support: %"PRIu32"\n", rf(1, 27, 24));
+	printf("Number of Semaphores: %"PRIu32"\n", rf(1, 23, 16));
+	printf("Number of TMUs per Slice: %"PRIu32"\n", rf(1, 15, 12));
+	printf("Number of QPUs per Slice: %"PRIu32"\n", rf(1, 11, 8));
+	printf("Number of Slices: %"PRIu32"\n", rf(1, 7, 4));
+	printf("V3D Revision: %"PRIu32"\n", rf(1, 3, 0));
 
 	return 0;
 }
