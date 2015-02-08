ca3d3524c639c3cf9eb0bcfbf46d59c0fa6c7623
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-02-08 13:25:29 +0900

    add useful macro to access V3D and change to use it

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 #define gt(n, sr) ((((1<<((n)))-1)<<((sr))))
 #define en(e, n, sr) ((((p[((e))]&((gt(((n)), ((sr)))))))>>((sr))))
+#define rf(offset, bit_from, bit_to) ((en(((offset)), ((bit_from))-((bit_to))+1, ((bit_to)))))
 
 int main()
 {
@@ -15,10 +16,10 @@ int main()
 	printf("p: %p\n", p);
 	printf("p[0]: 0x%08"PRIx32"\n", p[0]);
 
-	printf("V3D Technology Version: %"PRIu32"\n", en(0, 31-24+1, 24));
-	printf("V3D Id String: 0x%02"PRIx8" 0x%02"PRIx8" 0x%02"PRIx8"\n", en(0, 8, 16), en(0, 8, 8), en(0, 8, 0));
-	printf("V3D Id String: %c%c%c\n", en(0, 8, 0), en(0, 8, 8), en(0, 8, 16));
-	printf("VPM Memory Size: %"PRIu32"\n", en(1, 4, 28));
+	printf("V3D Technology Version: %"PRIu32"\n", rf(0, 31, 24));
+	printf("V3D Id String: 0x%02"PRIx8" 0x%02"PRIx8" 0x%02"PRIx8"\n", rf(0, 7, 0), rf(0, 15, 8), rf(0, 23, 16));
+	printf("V3D Id String: %c%c%c\n", rf(0, 7, 0), rf(0, 15, 8), rf(0, 23, 16));
+	printf("VPM Memory Size: %"PRIu32"\n", rf(1, 31, 28));
 
 	return 0;
 }
