9851982e6a9463c7057eb8fb095cc1722348b0fb
Terminus <i.can.speak.c.and.basic@gmail.com>
2015-02-08 14:54:20 +0900

    v3d_rw: add comments of offset addresses in the header

--- v3d_rw.h
+++ v3d_rw.h
@@ -4,8 +4,8 @@
 #include <stdint.h>
 
 	typedef enum {
-		V3D_TVER, V3D_IDSTR_V, V3D_IDSTR_3, V3D_IDSTR_D,
-		V3D_VPMSZ, V3D_HDRT, V3D_NSEM, V3D_TUPS, V3D_QUPS, V3D_NSLC, V3D_REV,
+		/* 0x00000 */ V3D_TVER, V3D_IDSTR_V, V3D_IDSTR_3, V3D_IDSTR_D,
+		/* 0x00004 */ V3D_VPMSZ, V3D_HDRT, V3D_NSEM, V3D_TUPS, V3D_QUPS, V3D_NSLC, V3D_REV,
 	} v3d_field_name_t;
 
 	uint32_t v3d_read(uint32_t *p, v3d_field_name_t fname);
